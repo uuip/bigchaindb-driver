ce3535667aa81d9389a73fb76351a044cd851f8c
uuip <uuip@users.noreply.github.com>
2022-10-14 14:29:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ with open('CHANGELOG.rst') as changelog_file:
 
 install_requires = [
     'requests>=2.20.0',
-    'cryptoconditions==0.8.0',
-    'pysha3~=1.0.2',
-    'python-rapidjson~=0.6.0',
-    'python-rapidjson-schema==0.1.1',
+    'cryptoconditions>=0.8.0',
+    'pysha3>=1.0.2',
+    'python-rapidjson>=0.6.0',
+    'python-rapidjson-schema>=0.1.1',
 ]
 
 tests_require = [
